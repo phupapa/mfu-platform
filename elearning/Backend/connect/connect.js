1068db031445eb36d2f35c93eece6cd22c93fc7e
phuwanart phuangbangpho <phuwanart.phuang@gmail.com>
2025-04-18 08:36:25 +0700

    เปลี่ยนไปใช้ createPool() แทน

--- elearning/Backend/connect/connect.js
+++ elearning/Backend/connect/connect.js
@@ -1,8 +1,8 @@
-const mysql = require("mysql2/promise"); // <- ใช้ promise version
+const mysql = require("mysql2/promise"); 
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,  // <-- ถ้า DATABASE_URL เป็น URI เช่น mysql://user:pass@host/db
+  uri: process.env.DATABASE_URL,  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
