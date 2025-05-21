70b217a588cd3ccf0fb27549c6dc9f124542c9af
phuwanart phuangbangpho <phuwanart.phuang@gmail.com>
2025-05-21 14:15:45 +0700

    update UAT

--- elearning/Frontend/postbuild.sh
+++ elearning/Frontend/postbuild.sh
@@ -10,9 +10,6 @@ mkdir -p dist/elearning
 
 # 3. Move required files into dist/elearning
 # Move only if files exist
-if [ -f dist/index.html ]; then
-  mv dist/index.html dist/elearning/
-fi
 
 if [ -d dist/assets ]; then
   mv dist/assets dist/elearning/
